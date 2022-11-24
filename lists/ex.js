abf48333693836349cb584064057345303f57635
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-11-24 18:21:51 +0100

    Solve part 1 & 2 of lists exercise

--- lists/ex.js
+++ lists/ex.js
@@ -1,3 +1,35 @@
 "use strict";
 
-// Put your code here! :)
+const constant = value => () => value;
+
+const add = (x, y) => x + y;
+
+const add2 = (fn1, fn2) => {
+  return add(fn1(), fn2());
+}
+
+const addnReduce = fns => {
+  return fns.reduce(
+    (acc, currentFn) => add2(constant(acc), currentFn),
+    0
+  )
+};
+
+const addnLoop = fns => {
+  let acc = 0;
+  for (const fn of fns) {
+    acc = add2(constant(acc), fn)
+  }
+  return acc;
+}
+
+const addnRecursive = ([first, second, ...rest]) => {
+  if (rest.length === 0) return add2(first, second);
+  return addnRecursive([() => add2(first, second), ...rest]);
+}
+
+console.log(add(constant(4)(), constant(14)()));
+console.log(addnReduce([constant(4), constant(8), constant(100)]));
+console.log(addnReduce([constant(4), constant(8)]));
+console.log(addnLoop([constant(4), constant(8), constant(100)]));
+console.log(addnRecursive([constant(4), constant(8), constant(100)]));
